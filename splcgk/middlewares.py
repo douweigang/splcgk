4e4c9ccc2a60dc8220f53aa7734976ab6df20a9b
vigang <31495528+douweigang@users.noreply.github.com>
2019-02-21 15:04:13 +0800

    Update middlewares.py

--- splcgk/middlewares.py
+++ splcgk/middlewares.py
@@ -1,29 +1,14 @@
-import random
 
-import pymysql
-
+from wit.util import get_random_proxy
 
 class ProxyMiddleWare(object):
     """docstring for ProxyMiddleWare"""
 
     def process_request(self, request, spider):
         '''对request对象加上proxy'''
-        proxys = self.get_random_proxy()
-        proxy = random.choice(proxys)
-        print("this is request ip:" + proxy)
-        request.meta['proxy'] = proxy
+        proxy = get_random_proxy()
+        # print(proxy)
+        # proxy = random.choice(proxys)
+        print("this is request ip:" + proxy.get("http"),request.url)
+        request.meta['proxy'] = proxy.get("http")
 
-    def get_random_proxy(self):
-        '''随机从文件中读取proxy'''
-        conn = pymysql.connect(host="localhost", user="root", passwd="123456", db="ips", charset="utf8")
-        cursor = conn.cursor()
-        sql = "select * from ip_pool  LIMIT 0,150"
-        cursor.execute(sql)
-        datas = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        ips = []
-        for data in datas:
-            ips.append('http://'+data[1])
-
-        return ips
